7301be92087adbbd251be7e4b72390774f07390f
devwiandj@gmail.com <devWianDJ@gmail.com>
2023-02-21 10:49:21 +0200

    added webpack config

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-// import logo from './logo.svg';
 import './App.css';
 import Header from './Features/Header/header'
 import Home from './Features/Home/home'
 import Subreddits from './Features/SubReddits/subReddits'
-import { useRef } from 'react';
 
 
-function App() {
+export function App() {
   return (
     <>
       <Header/>
